c1be88b4378ced5e9f70af2f36cba619fb55ba37
Hosanna <gabeoji01@gmail.com>
2026-01-13 14:01:56 +0100

    feat(api): refactor - imports cleanup

--- notifyhub-api/src/main/java/com/notifyhub/api/controller/InboundMessageController.java
+++ notifyhub-api/src/main/java/com/notifyhub/api/controller/InboundMessageController.java
@@ -6,9 +6,13 @@ import com.notifyhub.api.inbound.db.InboundMessageRepository;
 import com.notifyhub.api.inbound.domain.InboundMessageStatus;
 import com.notifyhub.api.service.InboundMessageService;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 

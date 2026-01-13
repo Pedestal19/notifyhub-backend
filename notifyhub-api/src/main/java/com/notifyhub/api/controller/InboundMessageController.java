3e17d1932500ee3b0b291be61fb37684cc709588
Hosanna Gabe-Oji <gabeoji01@gmail.com>
2026-01-13 14:02:53 +0100

    Feat/repo queries list endpoint (#6)
    * feat(api): add repository queries and add endpoint for list inbound messages
    
    * feat(api): refactor - move logic from controller
    
    * feat(api): refactor - sort finall by receivedAt
    
    * feat(api): refactor - imports cleanup

--- notifyhub-api/src/main/java/com/notifyhub/api/controller/InboundMessageController.java
+++ notifyhub-api/src/main/java/com/notifyhub/api/controller/InboundMessageController.java
@@ -2,21 +2,29 @@ package com.notifyhub.api.controller;
 
 import com.notifyhub.api.dto.InboundMessageIngestRequest;
 import com.notifyhub.api.dto.InboundMessageResponse;
+import com.notifyhub.api.inbound.db.InboundMessageRepository;
+import com.notifyhub.api.inbound.domain.InboundMessageStatus;
 import com.notifyhub.api.service.InboundMessageService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class InboundMessageController {
 
     private final InboundMessageService inboundMessageService;
+    private final InboundMessageRepository inboundMessageRepository;
 
-    public InboundMessageController(InboundMessageService inboundMessageService) {
+    public InboundMessageController(InboundMessageService inboundMessageService, InboundMessageRepository inboundMessageRepository) {
         this.inboundMessageService = inboundMessageService;
+        this.inboundMessageRepository = inboundMessageRepository;
     }
 
     @PostMapping("/v1/inbound-messages")
@@ -29,5 +37,13 @@ public class InboundMessageController {
     @ResponseStatus(HttpStatus.CREATED)
     public InboundMessageResponse ingestWebhook(@Valid @RequestBody InboundMessageIngestRequest request) {
         return inboundMessageService.ingest(request);
+    }
+
+    @GetMapping("/v1/inbound-messages")
+    public List<InboundMessageResponse> list(
+            @RequestParam(required = false) InboundMessageStatus status,
+            @RequestParam(defaultValue = "20") int limit
+    ){
+        return inboundMessageService.list(status, limit);
     }
 }

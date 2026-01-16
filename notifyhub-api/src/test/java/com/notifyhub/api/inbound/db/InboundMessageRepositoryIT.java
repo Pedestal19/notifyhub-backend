0639c2f2eff9cc7f6c898290b68d56ed42badff7
Hosanna <gabeoji01@gmail.com>
2026-01-16 23:37:16 +0100

    test: add test yml file, clean up unsed code

--- notifyhub-api/src/test/java/com/notifyhub/api/inbound/db/InboundMessageRepositoryIT.java
+++ notifyhub-api/src/test/java/com/notifyhub/api/inbound/db/InboundMessageRepositoryIT.java
@@ -6,31 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.OffsetDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-@Testcontainers
+@ActiveProfiles("test")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class InboundMessageRepositoryIT {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
-
-     @DynamicPropertySource
-     static void registerProps(DynamicPropertyRegistry registry) {
-         registry.add("spring.datasource.url", postgres::getJdbcUrl);
-         registry.add("spring.datasource.username", postgres::getUsername);
-         registry.add("spring.datasource.password", postgres::getPassword);
-     }
 
     @Autowired
     InboundMessageRepository repository;

db684122c23e9c54965405dfd125624507a7fb0c
Hosanna <gabeoji01@gmail.com>
2026-01-23 22:02:59 +0100

    worker code refactor, update testing doc

--- notifyhub-worker/src/test/java/com/notifyhub/worker/service/InboundMessageProcessorTest.java
+++ notifyhub-worker/src/test/java/com/notifyhub/worker/service/InboundMessageProcessorTest.java
@@ -4,6 +4,10 @@ import com.notifyhub.worker.inbound.db.InboundMessageEntity;
 import com.notifyhub.worker.inbound.db.InboundMessageRepository;
 import com.notifyhub.worker.inbound.domain.InboundMessageStatus;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
@@ -12,14 +16,24 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyIterable;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class InboundMessageProcessorTest {
+
+    @Mock
+    InboundMessageRepository inboundMessageRepository;
+
+    @InjectMocks
+    InboundMessageProcessor inboundMessageProcessor;
 
     @Test
     void processBatch_movesReceivedToProcessed() {
-        InboundMessageRepository inboundMessageRepository = mock(InboundMessageRepository.class);
-        InboundMessageProcessor inboundMessageProcessor = new InboundMessageProcessor(inboundMessageRepository);
 
         var m1 = InboundMessageEntity.builder()
                 .status(InboundMessageStatus.RECEIVED)

ee5a1053dbac28cb174765b2e6e1a3e53877b17e
Hosanna <gabeoji01@gmail.com>
2026-01-30 21:36:24 +0100

    refactor wildcard imports to single imports

--- notifyhub-worker/src/test/java/com/notifyhub/worker/service/SkipLockedIT.java
+++ notifyhub-worker/src/test/java/com/notifyhub/worker/service/SkipLockedIT.java
@@ -16,7 +16,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
